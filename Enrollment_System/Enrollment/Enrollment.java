92a1599f3afa6758f98f290bbcafd7dadc6a515c
Arnold <aiy99673120@gmail.com>
2023-05-30 21:05:00 +0800

    Revert "test"
    This reverts commit 410be8603d160ef090cdad1ed81e09284da0b241.

--- Enrollment_System/Enrollment/Enrollment.java
+++ Enrollment_System/Enrollment/Enrollment.java
@@ -17,25 +17,24 @@ public class Enrollment implements ConnectionToDatabase {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // Integer.parseInt(System.console().readLine())
-        String test = "";
-        // System.out.println("Enter name:");
-        // String name = scan.nextLine();
-        // System.out.println("Enter age:");
-        // int age = scan.nextInt();
-        // System.out.println("Enter email:");
-        // String email = scan.nextLine();
-        // System.out.println("Enter contact number:");
-        // String contact_Number = scan.nextLine();
-        // System.out.println("Enter course:");
-        // String course = scan.nextLine();
-        // System.out.println("Enter year:");
-        // String year = scan.nextLine();
-        // System.out.println("Enter type:");
-        // String type = scan.nextLine();
-        //
-        // Student student = new Student(name, age, email, contact_Number, course, year,
-        // type, 1);
-        Enrollment test = new Enrollment();
+
+//        System.out.println("Enter name:");
+//        String name = scan.nextLine();
+//        System.out.println("Enter age:");
+//        int age = scan.nextInt();
+//        System.out.println("Enter email:");
+//        String email = scan.nextLine();
+//        System.out.println("Enter contact number:");
+//        String contact_Number = scan.nextLine();
+//        System.out.println("Enter course:");
+//        String course = scan.nextLine();
+//        System.out.println("Enter year:");
+//        String year = scan.nextLine();
+//        System.out.println("Enter type:");
+//        String type = scan.nextLine();
+//
+//        Student student = new Student(name, age, email, contact_Number, course, year, type, 1);
+        Enrollment test  = new Enrollment();
         test.connect();
 
     }
