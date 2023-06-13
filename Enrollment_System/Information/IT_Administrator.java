260b131f22c9bd77b31e46acaabc67cbeeb767b6
Arnold <aiy99673120@gmail.com>
2023-06-13 11:43:18 +0800

    pc changes

--- Enrollment_System/Information/IT_Administrator.java
+++
@@ -1,8 +0,0 @@
-package Information;
-
-public class IT_Administrator extends Administrator {
-
-    public IT_Administrator(String firstName, String middleName, String lastName, int address_ID, int contact_info_ID) {
-        super(firstName, middleName, lastName, address_ID, contact_info_ID);
-    }
-}
