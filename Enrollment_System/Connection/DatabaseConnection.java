36238a69300707fe3a5f386d753d2f88785546fa
MarkRamilo <markjoshuapramilo0930@yahoo.com>
2023-06-23 10:13:35 +0800

    Revert "update 3"
    This reverts commit 37742f5594276f43f37fb02163e41b7b7a56e110.

--- Enrollment_System/Connection/DatabaseConnection.java
+++ Enrollment_System/Connection/DatabaseConnection.java
@@ -2,13 +2,22 @@ package Connection;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Random;
 
 public interface DatabaseConnection {
 
     default Connection connect() {
         System.out.println("Connecting to database...");
         Connection con = null;
+
 
 
             //return con = DriverManager.getConnection(url, "Arceus", "m67Ds#rAm6");
@@ -18,9 +27,13 @@ public interface DatabaseConnection {
         String url = "jdbc:mysql://localhost:3306/test4";
         // set url to local folder of database test7
         //String url = "jdbc:mysql:C:\\Users\\PC\\Documents\\NetBeansProjects\\Enrollment_System\\test7.db";
+
+        //String url = "jdbc:mysql://dusk.mysql.database.azure.com:3306/test_integration?useSSL=true";
+        
         try {
-            // return con = DriverManager.getConnection(url, "Arceus", "m67Ds#rAm6");
+            //return con = DriverManager.getConnection(url, "Arceus", "m67Ds#rAm6");
             return con = DriverManager.getConnection(url, "root", "");
+
 
         } catch (Exception e) {
             // TODO: handle exception
