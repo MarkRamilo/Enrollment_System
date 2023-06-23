37742f5594276f43f37fb02163e41b7b7a56e110
Arnold <aiy99673120@gmail.com>
2023-06-23 09:47:44 +0800

    update 3

--- Enrollment_System/Connection/DatabaseConnection.java
+++ Enrollment_System/Connection/DatabaseConnection.java
@@ -2,29 +2,21 @@ package Connection;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
-
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.Year;
-import java.util.ArrayList;
-import java.util.Random;
 
 public interface DatabaseConnection {
 
     default Connection connect() {
         System.out.println("Connecting to database...");
         Connection con = null;
-        //String url = "jdbc:mysql://dusk.mysql.database.azure.com:3306/test_integration?useSSL=true";
+        // String url =
+        // "jdbc:mysql://dusk.mysql.database.azure.com:3306/test_integration?useSSL=true";
         String url = "jdbc:mysql://localhost:3306/test7";
-
-        
+        // set url to local folder of database test7
+        //String url = "jdbc:mysql:C:\\Users\\PC\\Documents\\NetBeansProjects\\Enrollment_System\\test7.db";
         try {
-            //return con = DriverManager.getConnection(url, "Arceus", "m67Ds#rAm6");
+            // return con = DriverManager.getConnection(url, "Arceus", "m67Ds#rAm6");
             return con = DriverManager.getConnection(url, "root", "");
-
 
         } catch (Exception e) {
             // TODO: handle exception
